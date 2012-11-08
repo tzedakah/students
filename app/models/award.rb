a879aed8858926a0b812fd6dbf3799ca3a206947
Antonio Chinea <creative.studio.acts@gmail.com>
2012-11-08 18:53:54 -0500

    Supporting the relationship between tables

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,5 @@
 class Award < ActiveRecord::Base
   attr_accessible :name, :student_id, :year
+  # every award is linked to a student, through student_id 
+  belongs_to :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ActiveRecord::Base
   attr_accessible :date_of_birth, :family_name, :given_name, :grade_point_average, :middle_name, :start_date
   # a student can have many awards
   has_many :awards
+
+  def name
+    given_name+ " " + family_name
+  end
 end
