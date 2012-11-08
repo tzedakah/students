e62dc29db34842a5189f8832f0d2e5c617384367
Antonio Chinea <creative.studio.acts@gmail.com>
2012-11-08 15:52:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Students::Application.config.secret_token = 'e614cd39b02eb430b97949e77813d4570df58ee4f0174db2364682e7e956393d86bf2c1d9034bb1d862da1b8073ae5a1a4bf8a0b98cd8c1a004e3d61adb26359'
